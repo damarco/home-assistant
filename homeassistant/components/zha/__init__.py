60923f4bfacd1e03098e45e8ccce515fa76bd55a
damarco <schmidt.d@aon.at>
2018-10-07 20:09:17 +0200

    Fix node_config retrieval

--- homeassistant/components/zha/__init__.py
+++ homeassistant/components/zha/__init__.py
@@ -109,7 +109,7 @@ async def async_setup_entry(hass, config_entry):
 
     database = config_entry.data.get(CONF_DATABASE)
     APPLICATION_CONTROLLER = ControllerApplication(radio, database)
-    listener = ApplicationListener(hass, config_entry.data)
+    listener = ApplicationListener(hass, config_entry)
     APPLICATION_CONTROLLER.add_listener(listener)
     await APPLICATION_CONTROLLER.startup(auto_form=True)
 
@@ -232,8 +232,11 @@ class ApplicationListener:
             component = None
             profile_clusters = ([], [])
             device_key = "{}-{}".format(device.ieee, endpoint_id)
-            node_config = self._config_entry[DOMAIN][CONF_DEVICE_CONFIG].get(
-                device_key, {})
+            node_config = {}
+            if CONF_DEVICE_CONFIG in self._config_entry.data:
+                node_config = self._config_entry.data[CONF_DEVICE_CONFIG].get(
+                    device_key, {}
+                )
 
             if endpoint.profile_id in zigpy.profiles.PROFILES:
                 profile = zigpy.profiles.PROFILES[endpoint.profile_id]
